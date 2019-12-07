ac63e5c60fec1ff0582762eb98e7b74e5d82c9ae
ogseverino <ogseverino@gmail.com>
2019-12-07 00:40:14 -0400

    Updates

---
+++ precache-manifest.924046425157549e5285af0ba27bafca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64098a7d898ffc6eaba82eebc73e40e8",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "096d9914faa1ada93c2f",
+    "url": "/monstersRolodex/static/css/main.93b616a3.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monstersRolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstersRolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "096d9914faa1ada93c2f",
+    "url": "/monstersRolodex/static/js/main.fb4ce25c.chunk.js"
+  },
+  {
+    "revision": "69fe64bfba5e66daaa43",
+    "url": "/monstersRolodex/static/js/runtime-main.ed38300b.js"
+  }
+]);
